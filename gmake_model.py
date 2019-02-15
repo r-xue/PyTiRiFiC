correct the Jy/pix->Jy/beam normalization; add gmake_model_export()

gmake_model.py
@@ -54,7 +54,7 @@ def gmake_model_api(mod_dct,dat_dct,
                 
                 imodel=gmake_model_kinmspy(hd,obj)
                 
-            imodel=np.squeeze(imodel)
+            #imodel=np.squeeze(imodel)
             
             if  'imodel@'+image in models.keys():
                 models['imodel@'+image]+=imodel
@@ -67,7 +67,9 @@ def gmake_model_api(mod_dct,dat_dct,
                 models['sample@'+image]=sample
                 models['psf@'+image]=psf
                 
-    #   SECOND PASS: simulate observations IMAGE BY IMAGE
+    #   "OPTIONAL" SECOND PASS: simulate observations IMAGE BY IMAGE
+    
+    start_time = time.time()
     
     for tag in models.keys():
         
@@ -79,6 +81,8 @@ def gmake_model_api(mod_dct,dat_dct,
             models[tag.replace('imodel@','cmodel@')]=cmodel.copy()
             models[tag.replace('imodel@','kernel@')]=kernel.copy()
             
+    print("---{0:^10} : {1:<8.5f} seconds ---".format('simobs',time.time()-start_time))
+    
     return models                
 
 def gmake_model_lnlike(theta,fit_dct,inp_dct,dat_dct,
@@ -197,6 +201,34 @@ def gmake_model_lnlike(theta,fit_dct,inp_dct,dat_dct,
     lnl=blobs['lnprob']
     
     return lnl,blobs
+
+
+def gmake_model_export(models,outdir='./'):
+    """
+        export model into FITS
+    """
+    for key in models.keys(): 
+        
+        if  'data@' not in key:
+            continue
+        
+        basename=key.replace('data@','')
+        basename=os.path.basename(basename)
+        if  not os.path.exists(outdir):
+            os.makedirs(outdir)
+        versions=['data','imodel','cmodel','error','mask','kernel','psf','residual']
+        hd=models[key.replace('data@','header@')]
+        for version in versions:
+            if  version=='residual' and key.replace('data@','cmodel@') in models.keys():
+                fits.writeto(outdir+'/'+version+'_'+basename,
+                             models[key]-models[key.replace('data@','cmodel@')],
+                             models[key.replace('data@','header@')],
+                             overwrite=True)                
+            if  key.replace('data@',version+'@') in models.keys():
+                fits.writeto(outdir+'/'+version+'_'+basename,
+                             models[key.replace('data@',version+'@')],
+                             models[key.replace('data@','header@')],
+                             overwrite=True)
 
 
 def gmake_model_lnprior(theta,fit_dct):

gmake_model_func.py
@@ -153,8 +153,8 @@ def gmake_model_kinmspy(header,obj,
     cube=cube.T
     if  dv<0: cube=np.flip(cube,axis=0)
     
-    model=np.zeros((header['NAXIS3'],header['NAXIS2'],header['NAXIS1']))
-    model=paste_array(model,cube,(int(pz_o_int),int(py_o_int),int(px_o_int)))
+    model=np.zeros((header['NAXIS4'],header['NAXIS3'],header['NAXIS2'],header['NAXIS1']))
+    model=paste_array(model,cube[np.newaxis,:,:,:],(0,int(pz_o_int),int(py_o_int),int(px_o_int)))
     
     return model
 
@@ -171,6 +171,7 @@ def gmake_model_simobs(data,header,beam=None,psf=None,returnkernel=False):
     cell=np.mean(proj_plane_pixel_scales(WCS(header).celestial))*3600.0
     
     #   get the convolution Kernel normalized to 1 at PEAK
+    #   turn on normalization in convolve() will force the Jy/Pix->Jy/beam conversion
     #   priority: psf>beam>header(2D or 3D KERNEL) 
     
     if  psf is not None:
@@ -186,12 +187,14 @@ def gmake_model_simobs(data,header,beam=None,psf=None,returnkernel=False):
         kernel=makekernel(header['NAXIS1'],header['NAXIS2'],
                         [header['BMAJ']*3600./cell,header['BMIN']*3600./cell],pa=header['BPA'])
 
-    model=data.copy()
+    model=np.zeros_like(data)
     for i in range(header['NAXIS3']):
+        if  np.sum(data[0,i,:,:])==0.0:
+            continue
         if  kernel.ndim==2:
-            model[0,i,:,:]=convolve_fft(data[0,i,:,:],kernel)
+            model[0,i,:,:]=convolve_fft(data[0,i,:,:],kernel,normalize_kernel=False)
         else:
-            model[0,i,:,:]=convolve_fft(data[0,i,:,:],psf[0,i,:,:])
+            model[0,i,:,:]=convolve_fft(data[0,i,:,:],kernel[0,i,:,:],normalize_kernel=False)
 
     if  returnkernel==True:
         return model,kernel