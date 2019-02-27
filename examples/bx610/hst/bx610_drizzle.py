54ea4df4cbb9e2580b9e05823cd8a81fc20644f8
Rui Xue <rx.astro@gmail.com>
2019-02-27 11:47:20 -0600

    update the hst-imaging/drizzle script

--- examples/bx610/hst/bx610_drizzle.py
+++ examples/bx610/hst/bx610_drizzle.py
@@ -7,8 +7,11 @@ import numpy as np
 from drizzle import drizzle
 from drizzlepac import astrodrizzle
 import pyfits
+from drizzlepac import tweakreg
 
-def bx610_drizzle(infiles='',outname=''):
+def bx610_drizzle(infiles='',outname='',fixheader=False,coeffs=True,
+                  final_refimage='',
+                  pipeline=True):
     """
     
     Data Quality:
@@ -20,10 +23,9 @@ def bx610_drizzle(infiles='',outname=''):
     print("*"*100)
     print(flist,'-->',outname)
     print("*"*100)
+
     
-    #   quick solution
-    
-    if  not ('flt.fits' in infiles or 'flc.fits' in infiles) :
+    if  pipeline==False:
         
         #   reproject+meanstack
         
@@ -38,7 +40,7 @@ def bx610_drizzle(infiles='',outname=''):
         #pprint.pprint(stack)
         stack=np.array(stack)
         stack=np.mean(stack,axis=0)
-        fits.writeto(outname+'_stack_quickmean.fits',stack,header,overwrite=True)
+        fits.writeto(outname+'_quickmean.fits',stack,header,overwrite=True)
 
         #   direct drizzle solution
         
@@ -47,27 +49,62 @@ def bx610_drizzle(infiles='',outname=''):
         driz=drizzle.Drizzle(outwcs=reference_wcs)
         for flist0 in flist:
             driz.add_fits_file(flist0)
-        driz.write(outname+'_stack_drizzle.fits')
+        driz.write(outname+'_drizzle.fits')
 
     
     #   the pipeline solution: astrodrizzle
     #   Note:
     #       ext: http://www.stsci.edu/hst/wfc3/documents/handbooks/currentDHB/Chapter2_data_structure2.html
     #       flc-fix: http://www.stsci.edu/hst/wfc3/documents/newsletters/STAN_10_11_2013
-    if  'flt.fits' in infiles or 'flc.fits' in infiles:
-        if  'flc.fits' in infiles:
-            for flist0 in flist:
-                flc = pyfits.open(flist0,mode='update')
-                del flc['sci',1].header['d2imdis*']
-                flc.close()
-        astrodrizzle.AstroDrizzle(infiles,output=outname+'_stack',updatewcs=True,coeffs=True) 
-        
+    #       http://www.stsci.edu/hst/HST_overview/drizzlepac/examples
+    else:
+
+        if  fixheader==True:
+            if  'flc.fits' in infiles:
+                for flist0 in flist:
+                    flc = pyfits.open(flist0,mode='update')
+                    del flc['sci',1].header['d2imdis*']
+                    flc.close()
+    
+        astrodrizzle.AstroDrizzle(infiles,output=outname,coeffs=coeffs,
+                                  static=True,skysub=True,driz_separate=True,
+                                  #final_refimage=final_refimage,final_scale=0.06,
+                                  final_wcs=True,final_rot=0)
+        #updatewcs=True,coeffs=True
     
 if  __name__=="__main__":
     
-    #bx610_comb(infiles='n9nh06*mos.fits',outname='p10924')
-    #bx610_comb(infiles='icjl08*drc.fits',outname='p13669')
-    bx610_drizzle(infiles='icjl08*flc.fits',outname='p13669')
+    ###bx610_drizzle(infiles='n9nh06*mos.fits',outname='p10924')
+    ###bx610_drizzle(infiles='icjl08*drc.fits',outname='p13669')
+    
+    #"""
+    #   p13669-f438w (WFC3/UVIS1)
+    bx610_drizzle(infiles='p13669-f438w/icjl08*flc.fits',outname='stack_f438w')
+    #   p13669-f814w (ACS/WFC)
+    bx610_drizzle(infiles='p13669-f814w/jcjla8*flc.fits',outname='stack_f814w')
+    #   p14620-f140w (WFC3/IR, no flt)
+    bx610_drizzle(infiles='p14620-f140w/id7l04*flt.fits',outname='stack_f140w')
+    #   p12578-f110w (WFC3/IR, no flt)
+    bx610_drizzle(infiles='p12578-f110w/ibra20*flt.fits',outname='stack_f110w')
+    #"""    
+    
+    #"""
+    #   p10924-f160w 
+    #bx610_drizzle(infiles='n9nh06*cal.fits',outname='stack_cal',coeffs=False) # okay
+    bx610_drizzle(infiles='p10924-f160w/n9nh06*mos.fits',outname='stack_f160w',coeffs=False) # okay
+    #bx610_drizzle(infiles='n9nh06*mos.fits',outname='stack_mos',pipeline=False) # okay
+    #bx610_drizzle(infiles='n9nh06*cal.fits',outname='stack_cal',pipeline=False) # bad choice
+    #"""
+    
+    """
+    flist=['stack_f438w_drc_sci.fits','stack_f814w_drc_sci.fits']
+    tweakreg.TweakReg(flist,
+          imagefindcfg=     {'threshold':100, 'conv_width' : 3.5},
+          refimagefindcfg=  {'threshold':100, 'conv_width' : 3.5},
+          updatehdr=False, shiftfile=True, outshifts='shift.txt')    
+    """
+    
+    
     
     """
     F160W->p10924_stack_drizzle.fits / p10924_stack_drc_sci.fits p10924_stack_mean.fits
