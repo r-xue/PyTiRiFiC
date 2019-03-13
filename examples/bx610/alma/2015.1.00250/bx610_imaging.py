7e1f5922c82912e88682e0bc737f3fc9556a4cb3
Rui Xue <rx.astro@gmail.com>
2019-03-13 12:29:35 -0500

    add bx610_uvprep.py

--- examples/bx610/alma/2015.1.00250/bx610_imaging.py
+++ examples/bx610/alma/2015.1.00250/bx610_imaging.py
@@ -16,20 +16,7 @@ mstransform(outputvis='uid___A002_Xb64387_X1f95_target.ms',
 #concat(vis=['uid___A002_Xb638bc_X2aa8_target.ms','uid___A002_Xb64387_X1f95_target.ms'],concatvis='uid___A001_X2fe_X20f_target.ms')
 
 
-"""
-spw_list=['0','1','2','3']
-spwtag_list=['spw25','spw27','spw29','spw31']
 
-for ind in range(4):
-
-    os.system('rm -rf uid___A001_X2fe_X20f_target.'+spwtag_list[ind]+'.ms')
-    os.system('rm -rf uid___A001_X2fe_X20f_target.'+spwtag_list[ind]+'.uvfits')
-    mstransform(vis='uid___A001_X2fe_X20f_target.ms',outputvis='uid___A001_X2fe_X20f_target.'+spwtag_list[ind]+'.ms',timeaverage=True,timebin='30s',spw=spw_list[ind],datacolumn='data',chanaverage=True,chanbin=10,
-            keepflags=True)
-    initweights('uid___A001_X2fe_X20f_target.'+spwtag_list[ind]+'.ms',wtmode='weight',dowtsp=True)
-    exportuvfits('uid___A001_X2fe_X20f_target.'+spwtag_list[ind]+'.ms','uid___A001_X2fe_X20f_target.'+spwtag_list[ind]+'.uvfits')
-#
-"""
 
 execfile('/Users/Rui/Dropbox/Workspace/projects/alma_g09/scripts/alma_imager.py')
 
