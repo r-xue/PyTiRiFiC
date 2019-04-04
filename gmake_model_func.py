correct the model rotation bug; export imodel/imod2d/imod3d/ms

gmake_model_func.py
@@ -656,8 +656,8 @@ def gmake_model_uvsample(xymodel,header,uvdata,uvw,phasecenter,
             #ne2
             ne.evaluate("a+b",
                         local_dict={"a":uvmodel[:,i],
-                                    "b":sampleImage(xymodel[0,i,:,:],np.deg2rad(cell),(uvw[:,0]/wv),(uvw[:,1]/wv),                                   
-                                                    dRA=dRA,dDec=dDec,PA=0,check=False)
+                                    "b":sampleImage((xymodel[0,i,:,:]),np.deg2rad(cell),(uvw[:,0]/wv),(uvw[:,1]/wv),                                   
+                                                    dRA=dRA,dDec=dDec,PA=0,check=False,origin='lower')
                                     },
                         casting='same_kind',out=uvmodel[:,i])
             """
@@ -685,12 +685,14 @@ def gmake_model_uvsample(xymodel,header,uvdata,uvw,phasecenter,
         xymodelsum=xymodel.sum(axis=(0,2,3))
         xymodel_zscale=xymodelsum/xymodelsum[i0]        
         wv=const.c/(header['CDELT3']*i0+header['CRVAL3'])
-        uvmodel0=sampleImage(xymodel[0,i0,:,:],np.deg2rad(cell),
+        # check the pixel index /ra-dec mapping:
+        #       https://mtazzari.github.io/galario/tech-specs.html 
+        uvmodel0=sampleImage((xymodel[0,i0,:,:]),np.deg2rad(cell),
                                    #(uvw[:,0]/wv).copy(order='C'),
                                    #(uvw[:,1]/wv).copy(order='C'),
                                    (uvw[:,0]/wv),
                                    (uvw[:,1]/wv),                                   
-                                   dRA=dRA,dDec=dDec,PA=0,check=False)
+                                   dRA=dRA,dDec=dDec,PA=0,check=False,origin='lower')
         
         #tic0=time.time()
         

gmake_model_uv.py
@@ -292,14 +292,13 @@ def gmake_model_lnlike(theta,fit_dct,inp_dct,dat_dct,
     
     return lnl,blobs
 
-
+#@profile
 def gmake_model_export(models,outdir='./',outname_exclude=None,outname_replace=None):
     """
         export model into FITS
         shortname:    a string list to get rid of from the original data image name
         
     """
-
     for key in list(models.keys()): 
         
         if  'data@' not in key:
@@ -323,8 +322,7 @@ def gmake_model_export(models,outdir='./',outname_exclude=None,outname_replace=N
             
         versions=['data','imodel','cmodel','error','mask','kernel','psf','residual',
                   'imod2d','imod3d','cmod2d','cmod3d']
-        versions=['imodel'
-                  'imod2d','imod3d']        
+        versions=['imodel','imod2d','imod3d']        
         hd=models[key.replace('data@','header@')]
         for version in versions:             
             if  key.replace('data@',version+'@') in models.keys():
@@ -341,15 +339,19 @@ def gmake_model_export(models,outdir='./',outname_exclude=None,outname_replace=N
         for prof in list(models.keys()):
             
             if  'imod3d_prof@' in prof  and key.replace('data@','') in prof:
-
                 outname=prof.replace(key.replace('data@',''),'')
                 outname=outname.replace('imod3d_prof@','imodrp_').replace('@','_')
                 gmake_dct2fits(models[prof],outname=outdir+'/'+outname+basename.replace('.fits',''))
                 
 
-        basename=basename.replace('.fits','.ms')
-        os.system('cp -rf '+key.replace('data@','')+' '+outdir+'/data_'+basename)
-        add_uvmodel(outdir+'/data_'+basename,models[key.replace('data@','uvmodel@')]) 
+        oldms=key.replace('data@','')
+        newms=outdir+'/data_'+basename.replace('.fits','.ms')
+        
+        print("copy "+oldms+'  to  '+newms)
+        os.system("rm -rf "+newms)
+        os.system('cp -rf '+oldms+' '+newms)
+        
+        add_uvmodel(newms,models[key.replace('data@','uvmodel@')]) 
         
 
     np.save(outdir+'/'+'mod_dct.npy',models['mod_dct'])

gmake_utils.py
@@ -395,12 +395,36 @@ def gmake_pformat(fit_dct,verbose=True):
 def gmake_read_data(inp_dct,verbose=False,
                     fill_mask=False,fill_error=False):
     """
-    read data into the dictionary
+    read FITS/image or MS/visibilities into the dictionary
+    
+        + we set data=
+        
+        note: 
+              DATA column shape in nrecord x nchan x ncorr
+              WEIGHT column shape in nrecord x ncorr( supposely this is the "average" value of WEIGHT_SPECTRUM along the channle-axis
+              WEIGHT_SPECTRUM SHAPE in nrecord x nchan x ncorr
+              FLAGS shape in nrecord x nchan x ncorr
+              (so WEIGHT_SPECTRUM is likely ~WEIGHT/NCHAN?) depending on the data model / calibration script
+              
+              data@ms in complex64 (not complex128)
+        
+        For space-saving, we 
+            + set DATA-values=np.nan when flag=True (so there is no a "flag" variable for flagging in dat_dct
+            + when polaverage=True, we only derive stokes-I if both XX/YY (or RR/YY) are Good. If one of them are flagged, Data-Values set to np.nan
+                this follows the principle in the tclean()/stokes parameter 
+                https://casa.nrao.edu/casadocs-devel/stable/global-task-list/task_tclean/parameters
+                http://casacore.github.io/casacore/StokesConverter_8h_source.html
+            + weight doesn't include the channel-axis (assuming the channel-wise weight variable is neligible   
+            
+            XX=I+Q YY=I-Q ; RR=I+V LL=I-V => I=(XX+YY)/2 or (RR+LL)/2
+                
     """
+    
     dat_dct={}
     
     if  verbose==True:
         print("+"*80)
+    
     for tag in inp_dct.keys():
         if  'image' not in inp_dct[tag].keys():
             continue
@@ -474,29 +498,7 @@ def gmake_read_data(inp_dct,verbose=False,
 def gmake_read_ms(inp_dct,verbose=False,memorytable=True,
                   polaverage=True,
                   dataflag=True,saveflag=False):
-    """
-    read MS into dictionary
-        + we set data=
-        
-        note: 
-              DATA column shape in nrecord x nchan x ncorr
-              WEIGHT column shape in nrecord x ncorr( supposely this is the "average" value of WEIGHT_SPECTRUM along the channle-axis
-              WEIGHT_SPECTRUM SHAPE in nrecord x nchan x ncorr
-              FLAGS shape in nrecord x nchan x ncorr
-              (so WEIGHT_SPECTRUM is likely ~WEIGHT/NCHAN?) depending on the data model / calibration script
-              
-              data@ms in complex64 (not complex128)
-        
-        For space-saving, we 
-            + set DATA-values=np.nan when flag=True (so there is no a "flag" variable for flagging in dat_dct
-            + when polaverage=True, we only derive stokes-I if both XX/YY (or RR/YY) are Good. If one of them are flagged, Data-Values set to np.nan
-                this follows the principle in the tclean()/stokes parameter 
-                https://casa.nrao.edu/casadocs-devel/stable/global-task-list/task_tclean/parameters
-                http://casacore.github.io/casacore/StokesConverter_8h_source.html
-            + weight doesn't include the channel-axis (assuming the channel-wise weight variable is neligible   
-            
-            XX=I+Q YY=I-Q ; RR=I+V LL=I-V => I=(XX+YY)/2 or (RR+LL)/2
-    """
+
     dat_dct={}
     
     if  verbose==True:
@@ -614,18 +616,25 @@ def convert_size(size_bytes):
     #    gmake_lmfit_analyze(fit_dct,sampler['inp_dct'],sampler['inp_dct'],sampler['dat_dct'],nstep=nstep)
 
 
-def add_uvmodel(vis,uvmodel):
-
+def add_uvmodel(vis,uvmodel,removemodel=True):
+    """
+    + add corrected column to vis
+    + remove model_data column in vis
+    + remove imaging_weight in vis
+    
+    """
+    
+    ctb.addImagingColumns(vis, ack=False)
     #ctb.removeImagingColumns(vis)
-    ctb.addImagingColumns(vis, ack=True)
-
     t=ctb.table(vis,ack=False,readonly=False)
     tmp=t.getcol('DATA')
     t.putcol('CORRECTED_DATA',np.broadcast_to(uvmodel[:,:,np.newaxis],tmp.shape))
     t.removecols('IMAGING_WEIGHT')
-    t.removecols('MODEL_DATA')
+    if  removemodel==True:
+        t.removecols('MODEL_DATA')
     t.unlock()
 
+    return 
 
 if  __name__=="__main__":
     