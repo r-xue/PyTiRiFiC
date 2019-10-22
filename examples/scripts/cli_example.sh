2ed9267c12018f3818dbbec2d31324844daa4c32
Rui Xue <rx.astro@gmail.com>
2019-10-22 15:44:39 -0500

    update parameter definitation file

--- examples/scripts/cli_example.sh
+++ examples/scripts/cli_example.sh
@@ -4,6 +4,6 @@ gmake_cli ../inpfile/bx610_band6_uv_ab.inp --debug --fit        #   model fittin
 gmake_cli ../inpfile/bx610_band6_uv_ab.inp --debug --analyze    #   analyze / model exporting/imaging
 gmake_cli ../inpfile/bx610_band6_uv_ab.inp --debug --plot       #   diagnostic plots
 
-gmake_cli ../inpfile/bx610_band6_uv_mc.inp --debug --fit        #   model fitting
-gmake_cli ../inpfile/bx610_band6_uv_mc.inp --debug --analyze    #   analyze / model exporting/imaging
-gmake_cli ../inpfile/bx610_band6_uv_mc.inp --debug --plot       #   diagnostic plots
+gmake_cli ../inpfile/bx610_b6c3_uv_mc.inp --debug --fit        #   model fitting
+gmake_cli ../inpfile/bx610_b6c3_uv_mc.inp --debug --analyze    #   analyze / model exporting/imaging
+gmake_cli ../inpfile/bx610_b6c3_uv_mc.inp --debug --plot       #   diagnostic plots
